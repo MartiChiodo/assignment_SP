af9ddb75a20a620ecc8879c73d802765de2a3c1f
Martina <161827443+MartiChiodo@users.noreply.github.com>
2025-01-16 14:51:19 +0100

    some fix

--- main.py
+++ main.py
@@ -1,106 +1,120 @@
+
 import numpy as np
 from instances import *
-from solvers import *
-import json 
+from solvers import GRASP_Solver
 import random
 
-# impporting data from toy.json
-with open("./data/ihtc2024_test_dataset/test03.json") as input_f:
-    data = json.load(input_f)
-
-
-# Initializing the istances of the problem
-hospital = Hospital(data['rooms'], data['operating_theaters'] ,data['days'])
-nurses = {elem['id']: Nurse(elem) for elem in data['nurses']}
-pat = {elem['id']: Patient(elem, hospital) for elem in data['patients']}
-patients = { key: elem.encoding_agegroup(data['age_groups']) for key,elem in pat.items()}
-occ = {elem['id']: Occupant(elem, hospital) for elem in data['occupants']}
-occupants = { key: elem.encoding_agegroup(data['age_groups']) for key,elem in occ.items()}
-surgeons = {elem['id']: Surgeon(elem) for elem in data['surgeons']}
-weights = data['weights']
-
-p = Problem(surgeons, nurses, patients, occupants, hospital, weights, data['days'])
-   
-
-# the initial guess is generated randomly with a function
-random.seed(384)
-rnd_state = p.generating_feasible_state()
-
-
-# SOLVING THE OPT PROBLEM
-print('\n')
-print('*********************************')
-print('SOLVING THE OPTIMIZATION PROBLEM')
-solver = GRASP_Solver(p, rnd_state)
-best_solution, best_f = solver.solve(num_restart=1)
-
-print('Best obj function = ', best_f)
-
-p.objective_function(best_solution, opt=1)
-
-
-# writing the solution json file
-import json
- 
-# Data to be written
-dictionary = {}
-
-lista_pat = []
-for id_pat in patients.keys():
-    dict_admission_pat = best_solution.dict_admission[id_pat]
-    room = 'r'+ str(dict_admission_pat[2])
+def main():
     
-    if dict_admission_pat[1] == -1: 
-        dict_admission_pat[1] = 'none'
+    # impporting libraries
+    import os
+    import json 
+
+    file_path = "./data/ihtc2024_test_dataset/test03.json"
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"The file {file_path} does not exist.")
+    
+    with open(file_path) as input_f:
+        data = json.load(input_f)
+
+
+    # Initializing the istances of the problem
+    hospital = Hospital(data['rooms'], data['operating_theaters'] ,data['days'])
+    nurses = {elem['id']: Nurse(elem) for elem in data['nurses']}
+    pat = {elem['id']: Patient(elem, hospital) for elem in data['patients']}
+    patients = { key: elem.encoding_agegroup(data['age_groups']) for key,elem in pat.items()}
+    occ = {elem['id']: Occupant(elem, hospital) for elem in data['occupants']}
+    occupants = { key: elem.encoding_agegroup(data['age_groups']) for key,elem in occ.items()}
+    surgeons = {elem['id']: Surgeon(elem) for elem in data['surgeons']}
+    weights = data['weights']
+
+    p = Problem(surgeons, nurses, patients, occupants, hospital, weights, data['days'])
+    
+
+    # the initial guess is generated randomly with a function
+    random.seed(384)
+    rnd_state = p.generating_feasible_state()
+
+
+    # SOLVING THE OPT PROBLEM
+    print('\n')
+    print('*********************************')
+    print('SOLVING THE OPTIMIZATION PROBLEM')
+    solver = GRASP_Solver(p, rnd_state)
+    best_solution, best_f = solver.solve(num_restart=1)
+
+    print('Best obj function = ', best_f)
+
+    p.objective_function(best_solution, opt=1)
+
+
+    # writing the solution json file
+    import json
+    
+    # Data to be written
+    dictionary = {}
+
+    lista_pat = []
+    for id_pat in patients.keys():
+        dict_admission_pat = best_solution.dict_admission[id_pat]
+        room = 'r'+ str(dict_admission_pat[2])
         
-        dic_prv = {
-                "id": id_pat,
-                "admission_day": dict_admission_pat[1],
-             }  
-        
-    else:
-        dic_prv = {
+        if dict_admission_pat[1] == -1: 
+            dict_admission_pat[1] = 'none'
+            
+            dic_prv = {
                     "id": id_pat,
                     "admission_day": dict_admission_pat[1],
-                    "room": room,
-                    "operating_theater": dict_admission_pat[0]
                 }  
+            
+        else:
+            dic_prv = {
+                        "id": id_pat,
+                        "admission_day": dict_admission_pat[1],
+                        "room": room,
+                        "operating_theater": dict_admission_pat[0]
+                    }  
+        
+        lista_pat.append(dic_prv)
+
+    dictionary['patients'] = lista_pat   
+
+    lista_nurses = []
+    for id_n, id_nurses in enumerate(nurses.keys()):
+        dic_prv = {'id': id_nurses}
+        lista_assignments = []
+        
+        for id_s, sched in enumerate(nurses[id_nurses].working_shift):
+            rooms = best_solution.room_to_be_assigned[id_n][id_s]
+            rooms_right_syntax = ['r'+ str(elem) for elem in rooms ]
+            
+            if sched['shift'] == 0: shift = 'early'
+            elif sched['shift'] == 1: shift = 'late'
+            elif sched['shift'] == 2: shift = 'night'
+            
+            dict_ass = {
+                'day': sched['day'],
+                'shift': shift,
+                'rooms': rooms_right_syntax
+            }
+            
+            lista_assignments.append(dict_ass)
+        
+        dic_prv['assignments'] = lista_assignments
+            
+        lista_nurses.append(dic_prv)
+
+    dictionary['nurses'] = lista_nurses
+
+
+    # Serializing json
+    json_object = json.dumps(dictionary, indent=4)
     
-    lista_pat.append(dic_prv)
+    # Writing to sample.json
+    with open("results/solution.json", "w") as outfile:
+        outfile.write(json_object)
 
-dictionary['patients'] = lista_pat   
 
-lista_nurses = []
-for id_n, id_nurses in enumerate(nurses.keys()):
-    dic_prv = {'id': id_nurses}
-    lista_assignments = []
+if __name__ == '__main__':
     
-    for id_s, sched in enumerate(nurses[id_nurses].working_shift):
-        rooms = best_solution.room_to_be_assigned[id_n][id_s]
-        rooms_right_syntax = ['r'+ str(elem) for elem in rooms ]
-        
-        if sched['shift'] == 0: shift = 'early'
-        elif sched['shift'] == 1: shift = 'late'
-        elif sched['shift'] == 2: shift = 'night'
-        
-        dict_ass = {
-            'day': sched['day'],
-            'shift': shift,
-            'rooms': rooms_right_syntax
-        }
-        
-        lista_assignments.append(dict_ass)
-    
-    dic_prv['assignments'] = lista_assignments
-        
-    lista_nurses.append(dic_prv)
-
-dictionary['nurses'] = lista_nurses
-
-
-# Serializing json
-json_object = json.dumps(dictionary, indent=4)
- 
-# Writing to sample.json
-with open("results/solution.json", "w") as outfile:
-    outfile.write(json_object)
+    main()
--- main_stability.py
+++ main_stability.py
@@ -1,1 +1,9 @@
 # test for stability
+
+
+def main_stability():
+    print("This is the main function")
+
+
+if __name__ == '__main__':
+    main_stability()
